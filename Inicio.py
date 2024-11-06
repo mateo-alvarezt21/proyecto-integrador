101853ab84c9104805e97f1ab6ec0587a8ebda9b
samuel <samuelrojashurtado@gmail.com>
2024-11-06 13:50:48 -0500

    Primer commit

--- Inicio.py
+++ Inicio.py
@@ -5,27 +5,45 @@ st.set_page_config(layout="wide", page_title="Mapping Demo", page_icon="🌍")
 
 
 # Título y subtítulo
-st.title("Proyecto Integrador: [Nombre del Proyecto]")
-st.subheader("Un Viaje Creativo con [Nombre del Equipo]")
+st.title("Proyecto Integrador: Los Vagabundos")
+st.subheader("Un Viaje Creativo con Los Vagabundos")
 
 # Imagen de fondo
-image = Image.open("./static/proyecto integrador.png") 
+image = Image.open("./static/Los Vagabundos.png") 
 st.image(image, width=700, use_column_width=True)  
 
 # Integrantes
 st.header("Nuestro Equipo")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
-    st.write("**[Nombre del Integrante 1]**")
-    st.write("[Rol en el proyecto]")
+    st.image("./static/SAMUEL ROJAS HURTADO.jpg", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**Samuel Rojas**")
+    st.write("Descansar los ojos")
 
 with col2:
     st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
-    st.write("**[Nombre del Integrante 2]**")
-    st.write("[Rol en el proyecto]")
+    st.write("**Mateo Alvarez**")
+    st.write("Dormir")
+
+with col3:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**Johana**")
+    st.write("No Ir a Clase")
+
+col4, col5, = st.columns(2)
+
+with col4:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**Alexander Rubiano**")
+    st.write("No Hacer Nada")
+
+with col5:
+    st.image("./static/user.png", width=200)  # Reemplaza con la ruta de la foto
+    st.write("**Angello Gomez**")
+    st.write("No Ayudar")
+
 
 # Descripción del proyecto
 st.header("Sobre el Proyecto")
